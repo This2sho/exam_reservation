1ae3b96e35cdd850dd9f66d9609fd4940175545e
This2sho <zx00018@naver.com>
2025-03-20 02:52:05 +0900

    refactor: 메서드 분리 및 예외 변경

--- app/api/ReservationService.py
+++ app/api/ReservationService.py
@@ -1,5 +1,6 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
-from datetime import date
+from datetime import date, time
 
 from app.api.dto.request.CreateReservationRequest import CreateReservationRequest
 from app.api.dto.request.UpdateReservationRequest import UpdateReservationRequest
@@ -24,14 +25,8 @@ class ReservationService:
         self.user_repository = UserRepository(db)
 
     def create_reservation(self, request: CreateReservationRequest, user_id: int) -> CreateReservationResponse:
-        if not ReservationManagement.is_valid_date(request.reservation_date):
-            raise ValueError(f"{request.reservation_date}은 예약이 불가능합니다. 최소 3일 전에 신청 가능합니다.")
-
-        existing_reservations = self.reservation_repository.get_reservations_in_time_range(request.reservation_date,
-                                                                                           request.start_time,
-                                                                                           request.end_time)
-        if not ReservationManagement.is_reservation_possible(existing_reservations, request.num_participants):
-            raise ValueError("해당 시간대 예약이 가득 찼습니다.")
+        self.validate_reservation(request.reservation_date, request.start_time, request.end_time,
+                                  request.num_participants)
 
         reservation = Reservation(date=request.reservation_date, start_time=request.start_time,
                                   end_time=request.end_time, num_participants=request.num_participants,
@@ -39,14 +34,23 @@ class ReservationService:
         create = self.reservation_repository.create(reservation)
         return CreateReservationResponse(reservation_id=create.id)
 
+    def validate_reservation(self, reservation_date: date, start_time: time, end_time: time, num_participants: int):
+        if not ReservationManagement.is_valid_date(reservation_date):
+            raise ValueError(f"{reservation_date}은 예약이 불가능합니다. 최소 3일 전에 신청 가능합니다.")
+        existing_reservations = self.reservation_repository.get_reservations_in_time_range(reservation_date,
+                                                                                           start_time,
+                                                                                           end_time)
+        if not ReservationManagement.is_reservation_possible(existing_reservations, num_participants):
+            raise ValueError("해당 시간대 예약이 가득 찼습니다.")
+
     def find_reservations(self, user_id: int) -> ReservationsResponse:
         reservations = self.reservation_repository.get_by_user_id(user_id)
         response_data = [
             ReservationDetailResponse(
                 reservation_id=res.id,
                 date=res.date,
-                start_time= res.start_time,
-                end_time= res.end_time,
+                start_time=res.start_time,
+                end_time=res.end_time,
                 num_participants=res.num_participants,
                 status=res.status
             )
@@ -70,7 +74,7 @@ class ReservationService:
         user = self.user_repository.get_by_id(user_id)
 
         if not user.is_admin():
-            raise ValueError("관리자가 아닙니다.")
+            raise HTTPException(status_code=403, detail="권한이 없습니다.")
         reservations = self.reservation_repository.get_all()
         response_data = [
             AdminReservationDetailResponse(
@@ -95,8 +99,8 @@ class ReservationService:
         reservation.status = ReservationStatus.CONFIRMED
         self.reservation_repository.update(reservation)
 
-
     def modify_reservation(self, user_id: int, reservation_id: int, request: UpdateReservationRequest):
+        self.validate_reservation(request.date, request.start_time, request.end_time, request.num_participants)
         user = self.user_repository.get_by_id(user_id)
         reservation = self.reservation_repository.get_by_id(reservation_id)
         if not ReservationManagement.can_modify_reservation(user, reservation):
@@ -114,5 +118,5 @@ class ReservationService:
 
         if not ReservationManagement.can_cancel_reservation(user, reservation):
             raise ValueError("예약을 취소할 수 없습니다.")
-
-        self.reservation_repository.delete(reservation)
+        reservation.status = ReservationStatus.CANCELLED
+        self.reservation_repository.update(reservation)
